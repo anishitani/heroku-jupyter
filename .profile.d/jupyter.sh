df39ac6475c5ffe9785dc350e56d620880deecb5
Andre Nishitani <atoshio25@gmail.com>
2017-01-05 02:12:51 -0200

    Update Jupyter password.

--- .profile.d/jupyter.sh
+++ .profile.d/jupyter.sh
@@ -1,7 +1,5 @@
 source '/app/.profile.d/python.sh'
 
-echo "IPYTHON_PASSWORD=$IPYTHON_PASSWORD"
-if [[ -n "$IPYTHON_PASSWORD" ]]; then
+if [[ -n "${IPYTHON_PASSWORD:+_}" ]]; then
 	export IPYTHON_PASSWORD_HASH=$( python -c "from IPython.lib import passwd; print passwd('${IPYTHON_PASSWORD}')" )
 fi
-echo "IPYTHON_PASSWORD_HASH=$IPYTHON_PASSWORD_HASH"
--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: jupyter notebook --no-browser --ip=* --port=$PORT --NotebookApp.password=$IPYTHON_PASSWORD_HASH
-#web: echo Hello! $IPYTHON_PASSWORD_HASH
+web: jupyter notebook --no-browser --ip=* --port=$PORT
